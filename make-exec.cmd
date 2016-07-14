098df8bf29c47943cc126903af415a7ca1c1eb76
Dietz, Michael <Michael.Dietz@nordicsemi.no>
2016-07-14 11:07:00 +0200

    cleanup logging

--- make-exec.cmd
+++ make-exec.cmd
@@ -8,12 +8,17 @@ SET python_main=nrf5_multi_prog/nrf5_multi_prog.py
 
 REM TODO: Check Python version, pip, and set up virtual environment.
 
-pip install -r requirements.txt
-pip install pyinstaller
+echo pip installing python modules required by %python_main%...
+pip install -r requirements.txt > NUL
+
+echo pip installing pyinstaller...
+pip install pyinstaller > NUL
 
 IF EXIST %bin_dir% rm -rf %bin_dir%
 
+echo bundling %python_main% as %bin_name%.exe using pyinstaller...
 pyinstaller -F %python_main% --name %bin_name% --log-level ERROR --clean
 
+echo moving %bin_name% to %bin_dir%...
 mkdir %bin_dir%
 mv dist\%bin_name%.exe %bin_dir%
